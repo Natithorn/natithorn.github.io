a6dd0387df7a47ec8445a88a5cb5962e4ad1e71d
Natithorn <natithorn.s@kkumail.com>
2024-06-30 23:36:41 -0700

    update

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("This call from JS File");
+var i = 1;
+i = "test";
+
+let j = 2;
+const k = 3;
+
+function plus(a,b) {
+    return a+b;
+}
+const result = plus(1,2);
+
+console.log(i,j,k,result);
