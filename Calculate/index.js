2348210d15c7a0bc13637dc6f6fe38da3ed3fed2
Natithorn <natithorn.s@kkumail.com>
2024-07-01 00:12:25 -0700

    Update

--- Calculate/index.js
+++ Calculate/index.js
@@ -8,10 +8,12 @@ const k = 3;
 function plus(a,b) {
     const eInput1 = document.getElementById("input1").value;
     const eInput2 = document.getElementById("input2").value;
-    document.getElementById('output').value = eInput1 + eInput2;
+    var a = parseInt(eInput1);
+    var b = parseInt(eInput2);
+    const output = a + b;
+    document.getElementById('output').value = output;
     
 }
 
 const multip = (a,b) => {return a*b}
-
 console.log(i,j,k,plus(1,2),multip(2,2));
