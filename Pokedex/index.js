b9479f404d0ca38f964a9b34ca7e2e24289279c8
Natithorn <natithorn.s@kkumail.com>
2024-07-17 10:39:37 +0700

    ok

--- Pokedex/index.js
+++ Pokedex/index.js
@@ -21,18 +21,18 @@ const fetchData = async () => {
             throw new Error('Network response was not ok.');
         }
         const data = await response.json();
-        const pokemons = typeName || abilityName ? data.pokemon : data.results;
+        const pokemons = typeName || abilityName ? data.pokemon.slice(offset, offset + limit) : data.results;
         const pokemonContainer = document.getElementById('pokemon-container');
 
-        pokemons.slice(offset, offset + limit).forEach((entry, index) => {
+        pokemons.forEach((entry, index) => {
             const pokemon = entry.pokemon || entry;
             const pokemonId = pokemon.url.split("/")[6];
             const card = document.createElement('div');
             card.classList.add('col', 'col-3');
-        
+
             card.innerHTML = `
                 <div class="card">
-                    <img class="card-img-top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png">
+                    <img class="card-img-top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png" alt="${pokemon.name}">
                     <div class="card-body">
                         <h5 class="card-title text-capitalize">${pokemon.name}</h5>
                         <a href="detail/index.html?name=${pokemon.name}" class="btn btn-details">ข้อมูล</a>
